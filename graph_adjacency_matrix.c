6c8d0f9e114df4a3cd1e21c2fdd2874b0c01ca8f
LAKSH DOSHI <102138784+LAKSH3002@users.noreply.github.com>
2022-11-01 23:35:46 +0530

    Graph_representation_matrix

--- graph_adjacency_matrix.c
+++ graph_adjacency_matrix.c
@@ -1,45 +1,59 @@
-/* Adjacency Matrix representation of an undirected graph in C */  
-  
-#include <stdio.h>  
-#define V 4 /* number of vertices in the graph */  
-  
-/* function to initialize the matrix to zero */  
-void init(int arr[][V]) {  
-  int i, j;  
-  for (i = 0; i < V; i++)  
-    for (j = 0; j < V; j++)  
-      arr[i][j] = 0;  
-}  
-  
-/* function to add edges to the graph */  
-void insertEdge(int arr[][V], int i, int j) {  
-  arr[i][j] = 1;  
-  arr[j][i] = 1;  
-}  
-  
-/* function to print the matrix elements */  
-void printAdjMatrix(int arr[][V]) {  
-  int i, j;  
-  for (i = 0; i < V; i++) {  
-    printf("%d: ", i);  
-    for (j = 0; j < V; j++) {  
-      printf("%d ", arr[i][j]);  
-    }  
-    printf("\n");  
-  }  
-}  
-  
-int main() {  
-  int adjMatrix[V][V];  
-  
-  init(adjMatrix);  
-  insertEdge(adjMatrix, 0, 1);  
-  insertEdge(adjMatrix, 0, 2);  
-  insertEdge(adjMatrix, 1, 2);  
-  insertEdge(adjMatrix, 2, 0);  
-  insertEdge(adjMatrix, 2, 3);  
-  
-  printAdjMatrix(adjMatrix);  
-  
-  return 0;  
-}  
+/* Adjacency Matrix representation of an undirected graph in C */
+
+#include <stdio.h>
+#define V 4 /* number of vertices in the graph */
+
+/* function to initialize the matrix to zero */
+void init(int arr[][4])
+{
+    int i, j;
+    for (i = 0; i < 4; i++)
+        for (j = 0; j < 4; j++)
+            arr[i][j] = 0;
+}
+
+/* function to add edges to the graph */
+void insertEdge(int arr[][4])
+{
+    int edges;
+    printf("ENTER THE NUMBER OF EDGES YOUR GRAPH HAS\n");
+    scanf("%d",&edges);
+    int i,j, a, b;
+    for (i = 0; i <= edges; i++)
+    {
+        printf("ENTER THE VERTEX A AND B THAT ARE CONNECTED TO EACH OTHER AND FORM AN EDGE\n");
+        printf("ENTER VERTEX A\n");
+        scanf("%d", &a);
+        printf("ENTER VERTEX B\n");
+        scanf("%d", &b);
+        arr[a][b] = 1;
+        arr[b][a] = 1;
+    }
+}
+
+/* function to print the matrix elements */
+void printAdjMatrix(int arr[][4])
+{
+    int i, j;
+    for (i = 0; i < 4; i++)
+    {
+        printf("%d: ", i);
+        for (j = 0; j < 4; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int adjMatrix[4][4];
+
+    int a, b;
+    init(adjMatrix);
+    printAdjMatrix(adjMatrix);
+    insertEdge(adjMatrix);
+    printAdjMatrix(adjMatrix);
+    return 0;
+}
